4f0b53a9e8fb1baf5a302fe4a415f6227c5b25e1
lordpdg <45717795+lordpdg@users.noreply.github.com>
2018-12-09 21:48:45 +0900

    GitHub for git pull by DK Park!

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
+// Third : git commit --amend
 
 print("Hello World")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
