6bea8eb8c5968edfb4e5604d73700c671195174e
Dae Kyu Park <lordpdg1@gmail.com>
2018-12-10 22:40:30 +0900

    Revert "Update hello.py"
    This reverts commit e3cbd8ef00cc406775a133e09d1ecc5f1622261a.

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 // For command line git tutorial
 // First: GitHub modification
 // Second: Local repository modification
 // Third : git commit --amend
 
+=======
+>>>>>>> parent of e3cbd8e... Update hello.py
 print("Hello World")
 print("Tell Your World")
 print("Tell his world")
